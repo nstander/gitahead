884204410ecd445fdef18c5de07d200c41f23e16
Natasha Stander <natasha@scitools.com>
2022-04-14 14:07:40 -0600

    Modified script to show more output

--- runund.sh
+++ runund.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p ccout
+echo "Running Und"
 $STIHOME/und -db gitahead.und \
-  analyze -errors -sarif und_analyze.sarif \
+  analyze -sarif und_analyze.sarif \
   codecheck -sarif und_ccrecommended.sarif "SciTools' Recommended Checks" ccout/
